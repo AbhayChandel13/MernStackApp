605e2ecdc5166da29e1ca455bf2e5da73280b394
Abhay Chandel <97330477+AbhayChandel13@users.noreply.github.com>
2022-07-22 03:12:03 +0530

    login Route Created

--- server/router/auth.js
+++ server/router/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 require('../db/conn');
 const User = require('../model/userSchema');
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.send(`Hello Form the server Router js`);
 
 })
@@ -38,37 +38,61 @@ router.get('/',(req,res)=>{
 
 //Using Async and Await :--
 
-router.post('/register',async(req,res)=>{
-    const{ name,email,phone, work,password,cpassword} = req.body;
+router.post('/register', async (req, res) => {
+    const { name, email, phone, work, password, cpassword } = req.body;
 
-    if(!name || !email || !phone || !work || !password || !cpassword){
-        return res.status(422).json({error: "Please Filled the correct data "})
-    }
-
-    try{
-      const userExist  =await User.findOne({email :email})
-      if(userExist){
-        return res.status(422).json({error:"email already Exist"});
-    }
-    const user = new User({name,email,phone,work,password,cpassword});
-    const userRegister = await user.save()
-    if(userRegister){
-        res.status(201).json({message:"User Registered Successfully "});
-    }  else{
-        res.status(500).json({error:"failed to registered"});
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ error: "Please Filled the correct data " })
     }
 
+    try {
+        const userExist = await User.findOne({ email: email })
+        if (userExist) {
+            return res.status(422).json({ error: "email already Exist" });
+        }
+        const user = new User({ name, email, phone, work, password, cpassword });
+        const userRegister = await user.save()
+        if (userRegister) {
+            res.status(201).json({ message: "User Registered Successfully " });
+        } else {
+            res.status(500).json({ error: "failed to registered" });
+        }
+
     }
-    catch(err){
-            console.log(err);
+    catch (err) {
+        console.log(err);
     }
 
-    
+
     // console.log(name);
     // console.log(email);
     //console.log(req.body);
     //  res.json({message: req.body});
     // res.send('mera register page');
+})
+
+//login route:--
+router.post('/signin', async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ error: "Please fill data in both the field " })
+        }
+
+        const userLogin = await User.findOne({ email: email });
+        console.log(userLogin);
+        if (!userLogin) {
+            res.json({ error: "User Does Not Exits" });
+        } else {
+            res.json({ message: "User SignIn Successfully" });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+    // console.log(req.body);
+    // res.json({message:"awesome "});
 })
 
 
