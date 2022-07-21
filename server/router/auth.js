c8a00c0f3eca51735d5a040a5969e795ff7f6e93
Abhay Chandel <97330477+AbhayChandel13@users.noreply.github.com>
2022-07-22 02:14:22 +0530

    asyns and await added

--- server/router/auth.js
+++ server/router/auth.js
@@ -9,24 +9,58 @@ router.get('/',(req,res)=>{
 
 })
 
-router.post('/register',(req,res)=>{
+// router.post('/register',(req,res)=>{
+//     const{ name,email,phone, work,password,cpassword} = req.body;
+
+//     if(!name || !email || !phone || !work || !password || !cpassword){
+//         return res.status(422).json({error: "Please Filled the correct data "})
+//     }
+
+//     User.findOne({email :email})
+//     .then((userExist)=>{
+//         if(userExist){
+//             return res.status(422).json({error:"email already Exist"});
+//         }
+
+//         const user = new User({name,email,phone,work,password,cpassword});
+//         user.save().then(()=>{
+//             res.status(201).json({message:"User Registered Successfully "});
+//         }).catch((err)=>res.status(500).json({error:"failed to registered"}));
+//     }).catch((err)=>{console.log(err)});
+//     // console.log(name);
+//     // console.log(email);
+//     //console.log(req.body);
+//     //  res.json({message: req.body});
+//     // res.send('mera register page');
+// })
+
+
+router.post('/register',async(req,res)=>{
     const{ name,email,phone, work,password,cpassword} = req.body;
 
     if(!name || !email || !phone || !work || !password || !cpassword){
         return res.status(422).json({error: "Please Filled the correct data "})
     }
 
-    User.findOne({email :email})
-    .then((userExist)=>{
-        if(userExist){
-            return res.status(422).json({error:"email already Exist"});
-        }
+    try{
+      const userExist  =await User.findOne({email :email})
+      if(userExist){
+        return res.status(422).json({error:"email already Exist"});
+    }
+    const user = new User({name,email,phone,work,password,cpassword});
+    const userRegister = await user.save()
+    if(userRegister){
+        res.status(201).json({message:"User Registered Successfully "});
+    }  else{
+        res.status(500).json({error:"failed to registered"});
+    }
 
-        const user = new User({name,email,phone,work,password,cpassword});
-        user.save().then(()=>{
-            res.status(201).json({message:"User Registered Successfully "});
-        }).catch((err)=>res.status(500).json({error:"failed to registered"}));
-    }).catch((err)=>{console.log(err)});
+    }
+    catch(err){
+            console.log(err);
+    }
+
+    
     // console.log(name);
     // console.log(email);
     //console.log(req.body);
