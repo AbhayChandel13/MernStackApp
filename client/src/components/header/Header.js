57d1260a087b4015384ef79800f7133fa314ec20
Abhay Chandel <97330477+AbhayChandel13@users.noreply.github.com>
2022-07-28 00:59:08 +0530

    logout button toggle in Header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React,{ useContext} from 'react';
 import { NavLink } from 'react-router-dom';
+import { UserContext } from '../../App';
 // import logo from "../../images/logo.png";
 // import Dashboard from '../Dashboard';
 
 const Header = () => {
 
+    //const [show, setShow] = useState(false);
+    const { state, dispatch } = useContext(UserContext);
+
    const  handleclick=()=>{
         console.log("Clicked");
         window.confirm("Are You Sure, You want to Logout?");
-
     }
-    
-  return (
-    <>     
-      <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+
+    const RenderMenu=()=>{
+        if(state){
+            return(
+                <>
+                <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             {/* <!-- Navbar Brand--> */}
             <a className="navbar-brand ps-3" href="#">Wowz United </a>
             {/* <!-- Sidebar Toggle--> */}
@@ -40,6 +45,39 @@ const Header = () => {
                 </li> */}
             </ul>
         </nav>
+                </>
+            )
+        }else{
+            return(
+                <>
+                <>
+            <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            {/* <!-- Navbar Brand--> */}
+            <a className="navbar-brand ps-3" href="#">Wowz United </a>
+            {/* <!-- Sidebar Toggle--> */}
+            <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i className="fas fa-bars"></i></button>
+            {/* <!-- Navbar Search--> */}
+            <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+                <div className="input-group">
+                    <input className="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                    <button className="btn btn-primary" id="btnNavbarSearch" type="button"><i className="fas fa-search"></i></button>
+                </div>
+            </form>
+           {/* -- Navbar-- */}
+           {/* <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+            <NavLink className="btn btn-primary btn-block" to="/logout" onClick={handleclick}>Logout</NavLink> 
+           
+            </ul> */}
+        </nav>
+            </>
+                </>
+            )
+        }
+    }
+    
+  return (
+    <> 
+      <RenderMenu />
       
     </>
   )
