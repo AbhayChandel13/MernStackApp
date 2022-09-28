8c251ea67a1c3d317243720e374fd0e073d54032
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-09-29 00:45:14 +0530

    formatting

--- client/src/reducer/UseReducer.js
+++ client/src/reducer/UseReducer.js
@@ -1,9 +1,8 @@
 export const initialState = null;
 
-export const reducer = (state,action)=> {    
-
-    if (action.type==="USER"){
-        return action.payload;
-    }
-    return state;
-}
+export const reducer = (state, action) => {
+  if (action.type === "USER") {
+    return action.payload;
+  }
+  return state;
+};
