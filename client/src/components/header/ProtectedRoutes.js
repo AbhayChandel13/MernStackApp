626c6a74ad862dbc3f71404c333737c24bf7f40e
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-09-29 00:44:04 +0530

    Formatting

--- client/src/components/header/ProtectedRoutes.js
+++ client/src/components/header/ProtectedRoutes.js
@@ -1,9 +1,9 @@
 import React from "react";
-import {Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ user, children }) => {
-    let User = JSON.parse(localStorage.getItem("user_login"));
-  
+  let User = JSON.parse(localStorage.getItem("user_login"));
+
   if (!User) {
     return <Navigate to="/" replace />;
   }
