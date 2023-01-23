ac84106b26676c6b43029c3717999b43ab44f1c5
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-01-23 20:03:53 +0530

    code formatting

--- server/app.js
+++ server/app.js
@@ -1,30 +1,27 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose'); 
-const express  = require('express');
-const cookieParser = require('cookie-parser');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 
-
 app.use(cookieParser());
-dotenv.config({path: './config.env'});
-require('./db/conn');
+dotenv.config({ path: "./config.env" });
+require("./db/conn");
 
 app.use(express.json());
 
-//we link the router files to make our  route easy 
-app.use(require('./router/auth'));
+//we link the router files to make our  route easy
+app.use(require("./router/auth"));
 
 const PORT = process.env.PORT;
 
 // const DB = 'mongodb+srv://AbhayChandel:chandelabhay@cluster0.w752yjc.mongodb.net/mernstack?retryWrites=true&w=majority';
 
-
-
-app.get('/contact',(req,res)=>{
-    res.cookie("Test",'Abhay');
-   res.send('Hello from the contact page ');
+app.get("/contact", (req, res) => {
+  res.cookie("Test", "Abhay");
+  res.send("Hello from the contact page ");
 });
 
-app.listen(PORT,()=>{
-    console.log(`server is running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
