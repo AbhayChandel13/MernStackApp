dcfcb6ffa3f3648c904f2b0b86e2e8971dbde08a
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-01-24 01:41:30 +0530

    code formatting

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,26 +1,29 @@
 const jwt = require("jsonwebtoken");
 const User = require("../model/userSchema");
 
-const Authenticate = async (req,res,next) =>{
+const Authenticate = async (req, res, next) => {
   try {
     const token = req.cookies.jwtoken;
-    const verifyToken = jwt.verify(token,process.env.SECRET_KEY );
+    const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
 
-    const rootUser = await User.findOne({id: verifyToken._id,"tokens.token":token});
+    const rootUser = await User.findOne({
+      id: verifyToken._id,
+      "tokens.token": token,
+    });
 
-    if(!rootUser) { throw new Error('User not found')}
+    if (!rootUser) {
+      throw new Error("User not found");
+    }
 
     req.token = token;
     req.rootUser = rootUser;
     req.userID = rootUser._id;
-    
-    next();
 
+    next();
   } catch (error) {
-    res.status(401).send('Unauthorized : No token provided')
+    res.status(401).send("Unauthorized : No token provided");
     console.log(error);
   }
-
-}
+};
 
 module.exports = Authenticate;
