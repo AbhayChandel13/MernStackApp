6da1b3f0bf5a262eff90b8bf39e29abb8983a6cc
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-01-30 20:43:04 +0530

    comment added

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -80,6 +80,15 @@ const Header = () => {
                         <li><a className="dropdown-item" href="#!">Logout</a></li>
                     </ul>
                 </li> */}
+                  {/* <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i className="fas fa-user fa-fw"></i></a>
+                    <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a className="dropdown-item" href="#!">Settings</a></li>
+                        <li><a className="dropdown-item" href="#!">Activity Log</a></li>
+                        <li><hr className="dropdown-divider" /></li>
+                        <li><a className="dropdown-item" href="#!">Logout</a></li>
+                    </ul>
+                </li> */}
             </ul>
           </nav>
         </>
