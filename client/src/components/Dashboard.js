32c2510079c08a87b8b45437459a01ff18256d1b
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-02-09 20:01:02 +0530

    code format

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -24,10 +24,6 @@ const Dashboard = () => {
       //     const error = new Error(res.error)
       //     throw error;
       // }
-      
-      
-
-      
     } catch (error) {
       console.log(error);
     }
