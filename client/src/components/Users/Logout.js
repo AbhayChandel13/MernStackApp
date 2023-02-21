fc922e959ff153f270b943cc689d66ba4505a22a
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-02-21 20:30:15 +0530

    code format

--- client/src/components/Users/Logout.js
+++ client/src/components/Users/Logout.js
@@ -1,41 +1,42 @@
-import React, { useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { UserContext } from '../../App';
+import React, { useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../App";
 
 const Logout = () => {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const { state, dispatch } = useContext(UserContext);
+  const { state, dispatch } = useContext(UserContext);
 
-    useEffect(() => {
-        fetch('/logout', {
-            method: "GET",
-            headers: {
-                Accept: "appllication/json",
-                "Content-Type": "application/json"
-            },
-            credentials: "include"
-        }).then((res) => {
-            dispatch({ type: "USER", payload: false })
-            //dispatch({type:"NoUSER"})
-            //navigate('/login',{replace : true});
-            if (res.status !== 200) {
-                const error = new Error(res.error);
-                throw error;
-            }
-            else {
-                navigate('/login', { replace: true });
-            }
-        }).catch((err) => {
-            console.log(err);
-        })
+  useEffect(() => {
+    fetch("/logout", {
+      method: "GET",
+      headers: {
+        Accept: "appllication/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
     })
+      .then((res) => {
+        dispatch({ type: "USER", payload: false });
+        //dispatch({type:"NoUSER"})
+        //navigate('/login',{replace : true});
+        if (res.status !== 200) {
+          const error = new Error(res.error);
+          throw error;
+        } else {
+          navigate("/login", { replace: true });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
-    return (
-        <>
-            <h1>Logout Page </h1>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Logout Page </h1>
+    </>
+  );
+};
 
-export default Logout
+export default Logout;
