cd666e8f70f0c57ae5868e558f0c99a320bf5cd4
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-02-22 20:03:41 +0530

    comment removed

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -24,11 +24,7 @@ const Dashboard = () => {
       //     const error = new Error(res.error)
       //     throw error;
       // }
-      // if(!res.status===200){
-      //     const error = new Error(res.error)
-      //     throw error;
-      // }
-      
+  
     } catch (error) {
       console.log(error);
     }
