c83641e9cdf3ec18ec3029a50baacb43e5eaa63e
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-03-03 20:21:06 +0530

    empty

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -42,6 +42,7 @@
 //                             </a>
 //                             <div class="sb-sidenav-menu-heading">Interface</div>
 
+
 //                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
 //                                 <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
 //                                 Pages
