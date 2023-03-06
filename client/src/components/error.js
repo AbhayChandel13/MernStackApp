58e3a2c6af3f392dbc6ad7ca3929f7d414faf199
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-03-06 20:09:38 +0530

    edited

--- client/src/components/error.js
+++ client/src/components/error.js
@@ -9,7 +9,7 @@ const Error = () => {
       {/* <div className="container">
         <div className="error d-flex flex-column justify-content-lg-center align-items-center">
           //<img src="./404.png" alt="error" className='errorimg' />
-          <h4 className="mt-10">404 Error ! Page Not Found 😭</h4>
+          <h4 className="mt-10">404 Error !Page Not Found 😭</h4>
           <button className="btn btn-primary mt-10" onClick={() => history("/")}>
             Redirect Login Page
           </button>
